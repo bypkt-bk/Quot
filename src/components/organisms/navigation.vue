8dc331e96cee1df09072f3a9df01dbdf2e2e446b
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2025-03-27 21:58:59 +0700

    feat(home): add navigation bar

---
+++ src/components/organisms/navigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navigation">
+    <button class="nav-btn">
+      <Home />
+      <p>Home</p>
+    </button>
+    <button class="nav-btn">
+      <Archive />
+      <p>Item</p>
+    </button>
+    <button class="nav-btn">
+      <ChartPie />
+      <p>Dashboard</p>
+    </button>
+  </nav>
+</template>
+
+<script setup>
+import { Home, Archive, ChartPie } from "lucide-vue-next";
+</script>
+
+<style>
+.navigation {
+  display: flex;
+  padding: 28px 14px;
+  flex-direction: column;
+  align-items: center;
+  width: fit-content;
+  gap: 20px;
+  border-radius: 20px;
+  border: 1px solid #3c3c3c;
+  background: #242424;
+}
+
+.nav-btn {
+  display: flex;
+  width: 62px;
+  height: 62px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+}
+.nav-btn p {
+  font-family: Onest;
+  font-size: 12px;
+}
+</style>

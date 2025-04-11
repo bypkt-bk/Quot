5b4d8d4e3761bef9048884efc19e7341cd6d4d02
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-04-11 18:22:24 +0700

    merge dev

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,39 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 export const prerender = false; // Disable static generation for this API route
 
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "../../../server/trpc";
 
 export const ALL = async ({ request }: { request: Request }) => {
-  return fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req: request,
-    router: appRouter,
-  });
-};
-=======
-=======
-export const prerender = false; // Disable static generation for this API route
-
->>>>>>> cb3c65e (fix: build app)
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { appRouter } from "../../../server/trpc";
-
-export const ALL = async ({ request }: { request: Request }) => {
-<<<<<<< HEAD
 	return fetchRequestHandler({
 		endpoint: "/api/trpc",
 		req: request,
 		router: appRouter,
 	});
 };
->>>>>>> 5f18958 (feat: api)
-=======
-  return fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req: request,
-    router: appRouter,
-  });
-};
->>>>>>> cb3c65e (fix: build app)

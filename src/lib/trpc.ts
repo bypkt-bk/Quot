2f131b39e96d22ac8b4cb2b5e8c989d22ebb47fc
Phongsakorn P. <135941130+Phongsakorn0@users.noreply.github.com>
2025-04-17 01:28:03 +0700

    Update trpc.ts

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -6,7 +6,7 @@ import { httpBatchLink } from "@trpc/client";
 export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "https://quot-vert.vercel.app",
+      url: "https://quot-vert.vercel.app/api/trpc",
     }),
   ],
 });
